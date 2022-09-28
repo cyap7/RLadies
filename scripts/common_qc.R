a5f4604423ddd263f5128494999619c4d3b98fa6
cyap7 <cyap7@users.noreply.github.com>
2022-09-28 17:40:57 +1000

    Upload

--- scripts/common_qc.R
+++
@@ -1,6 +0,0 @@
-# Common QC script
-
-# Make column of Brand + change colname to Make
-mtcars <- mtcars %>% 
-  dplyr::mutate(Brand = unlist(lapply(strsplit(as.character(mtcars$V1), "\\ "), function(x) x[[1]])), .before = V1) %>%
-  dplyr::rename(Make = V1)
